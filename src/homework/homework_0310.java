67a7d9133332c74da9663fa91eb3de71d6ac8a6b
damy <ekamiss22@gmail.com>
2021-03-10 12:55:19 +0900

    array for문

--- src/homework/homework_0310.java
+++ src/homework/homework_0310.java
@@ -1,14 +1,14 @@
 package homework;
 
-
 public class homework_0310 {
 	public static void main(String[] args) {
-		/*back-end 과제 
-		2중for문으로 구구단 출력하기*/
-		
-		for(int j=2;j<10;j++) {
-			for(int i=1;i<10;i++) 
-				System.out.printf("%d*%d=%d ", j, i, i*j);
+		/*
+		 * back-end 과제 2중for문으로 구구단 출력하기
+		 */
+
+		for (int j = 2; j < 10; j++) {
+			for (int i = 1; i < 10; i++)
+				System.out.printf("%d*%d=%d ", j, i, i * j);
 			System.out.println();
 		}
 	}
