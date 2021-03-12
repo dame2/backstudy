302f756ed77bf4d987b05693798fb7c59fb2d12f
damy <ekamiss22@gmail.com>
2021-03-12 13:20:21 +0900

    array

--- src/homework/homework_0312.java
+++ src/homework/homework_0312.java
@@ -25,10 +25,12 @@ public class homework_0312 {
 				}
 				realTotal[i] = total;
 			}
-		
-		System.out.printf(
-				"원하시는 합계의 번호를 입력해 주세요\n———————————————\n[1]%s\n[2]%s\n[3]%s\n[4]%s\n[5]%s\n[6]%s\n[7]%s\n[8]%s\n———————————————\n입력: ",
-				title[0], title[1], title[2], title[3], title[4], title[5], title[6], title[7]);
+			
+		System.out.println("원하시는 합계의 번호를 입력해 주세요.");
+		System.out.println("———————————————");
+				for(int i=1; i<8; i++)
+					System.out.printf("[%d] %s\n", i, title[i]);
+		System.out.print("———————————————\n입력: ");
 		number = nscan.nextInt();
 		while (number < 1 || number > 8) {
 			System.out.print("범위에 없는 번호입니다. 다시 입력해 주세요.\n입력: ");
