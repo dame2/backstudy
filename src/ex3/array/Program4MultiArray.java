8d3534c5ab3f61737ec1d5499b6476b3d9c38339
damy <ekamiss22@gmail.com>
2021-03-16 14:10:11 +0900

    multiArray

--- src/ex3/array/Program4MultiArray.java
+++ src/ex3/array/Program4MultiArray.java
@@ -41,7 +41,7 @@ public class Program4MultiArray {
 
 			for (int j = 0; j < count; j++) {
 				line = fscan.nextLine();
-				rows[j] = line.split(","); //왜 1차원 배열로 받는지 기존 count값을 넣은 2차원 배열은 어떻게 되는
+				rows[j] = line.split(","); // 왜 1차원 배열로 받는지 기존 count값을 넣은 2차원 배열은 어떻게 되는
 			}
 			fscan.close();
 			fis.close();
@@ -49,7 +49,7 @@ public class Program4MultiArray {
 
 		// 레코드를 출력하는 코드
 		for (int j = 0; j < count; j++) {
-			for (int i = 0; i < 10; i++) { //split한 rows 값이 어떻게 여기에 들어가는지
+			for (int i = 0; i < 10; i++) { // split한 rows 값이 어떻게 여기에 들어가는지
 				System.out.print(rows[j][i]);
 				if (i < 10 - 1)
 					System.out.print(",");
@@ -67,5 +67,43 @@ public class Program4MultiArray {
 				total += Integer.parseInt(rows[j][3]);
 			System.out.println(total);
 		}
+
+		// 일 가장 많은 검사진행수는?
+		// 그 max의 날짜[i][0]는?
+		int max = 0;
+		String date = "";
+		int idx = 0;
+		int current;
+
+		for (int i = 0; i < count; i++) {
+			current = Integer.parseInt(rows[i][3]);
+			if (current > max) {
+				max = current;
+				idx = i;
+				date = rows[idx][0];
+			}
+		}
+
+		System.out.printf("일별 가장 많은 검사 진행자수: %d", max);
+		System.out.println();
+		System.out.printf("그날: %s", date);
+		System.out.println();
+
+		// 확진자 수가 늘어난 일자와 수를 출력하시오.
+		int diff = 0;
+		current = Integer.parseInt(rows[0][1]);
+		
+		for (int i = 1; i < count; i++) {
+			int next = Integer.parseInt(rows[i][1]);
+			
+			if (next != current) {
+				diff = next - current;
+				date = rows[i][0];
+				current = next;
+				System.out.printf("%d: %s, %d\n", i+1, date, diff);
+			}
+
+		} 
+		
 	}
 }
---
+++ src/homework/homework_0316.java
@@ -0,0 +1,63 @@
+package homework;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class homework_0316 {
+
+	public static void main(String[] args) throws IOException {
+
+		int count = 0;
+		String[][] rows;
+		{
+			FileInputStream fis = new FileInputStream("res/covid19.csv");
+
+			Scanner fscan = new Scanner(fis);
+			fscan.nextLine();
+
+			// count수 구하기
+			while (fscan.hasNextLine()) {
+				count++;
+				fscan.nextLine(); // line에 담을 필요 없다
+			}
+			fscan.close();
+			fis.close();
+		}
+
+		// 2차원 배열에 값 저장하기
+		{
+			FileInputStream fis = new FileInputStream("res/covid19.csv");
+			Scanner fscan = new Scanner(fis);
+			String line;
+			
+			fscan.nextLine();
+			
+			rows = new String[count][];
+			for (int i = 0; i < count; i++) {
+				line = fscan.nextLine();
+				rows[i] = line.split(",");
+			}
+			fscan.close();
+			fis.close();
+		}
+		
+		// 확진자가 제일 많은 날을 출력하시오.
+		int recent = Integer.parseInt(rows[0][1]);
+		int diff = 0;
+		int idx = 0;
+		int max = 0;
+		for (int i = 0; i < count; i++) {
+			int newRecent = Integer.parseInt(rows[i][1]);
+			diff = newRecent - recent;
+			if (diff > max) {
+				max = diff;
+				idx = i;
+			}
+		}
+		
+		System.out.printf("확진자가 제일 많은 날: %d", rows[idx][0]);
+		System.out.printf("확진자 수: %d", rows[idx][0]);
+
+	}
+}
